5e0a09069838823c696a4c3a413c8baf70541e1e
Anson Guo <hi.jingan@gmail.com>
2021-03-11 12:22:36 +0800

    feat(home): 代码格式化

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,46 +13,19 @@ class HomePageState extends State<HomePage> {
   @override
   void initState() {
     routeList = [
+      {'label': '2.1 计数器应用示例', 'routeName': 'chapter_02_2_1_counter'},
+      {'label': '2.2 路由管理', 'routeName': 'chapter_02_2_2_route_management'},
+      {'label': '3.3 文本及样式', 'routeName': 'chapter_03_3_3_text'},
+      {'label': '3.4 按钮', 'routeName': 'chapter_03_3_4_button'},
+      {'label': '3.5 图片及icon', 'routeName': 'chapter_03_3_5_image_and_icon'},
       {
-        'label': '2.1 计数器应用示例', //
-        'routeName': 'chapter_02_2_1_counter'
-      },
-      {
-        'label': '2.2 路由管理', //
-        'routeName': 'chapter_02_2_2_route_management'
-      },
-      {
-        'label': '3.3 文本及样式', //
-        'routeName': 'chapter_03_3_3_text'
-      },
-      {
-        'label': '3.4 按钮', //
-        'routeName': 'chapter_03_3_4_button'
-      },
-      {
-        'label': '3.5 图片及icon', //
-        'routeName': 'chapter_03_3_5_image_and_icon'
-      },
-      {
-        'label': '3.6 单选开关和复选框', //
+        'label': '3.6 单选开关和复选框',
         'routeName': 'chapter_03_3_6_switch_and_checkbox'
       },
-      {
-        'label': '3.7 输入框', //
-        'routeName': 'chapter_03_3_7_textfield'
-      },
-      {
-        'label': '3.7 表单', //
-        'routeName': 'chapter_03_3_7_form'
-      },
-      {
-        'label': '3.8 进度指示器', //
-        'routeName': 'chapter_03_3_8_process_indicator'
-      },
-      {
-        'label': 'demo_01 微信读书卡片布局', //
-        'routeName': 'demo_01_wechat_reader'
-      },
+      {'label': '3.7 输入框', 'routeName': 'chapter_03_3_7_textfield'},
+      {'label': '3.7 表单', 'routeName': 'chapter_03_3_7_form'},
+      {'label': '3.8 进度指示器', 'routeName': 'chapter_03_3_8_process_indicator'},
+      {'label': 'demo_01 微信读书卡片布局', 'routeName': 'demo_01_wechat_reader'},
     ];
     super.initState();
   }
